6a26b060ce3d586daf5f065737fa52523ebb0b3c
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-12-07 02:24:37 -0700

    initial minimal viable product

--- src/index.js
+++ src/index.js
@@ -1,2 +1,108 @@
 import 'normalize.css';
 import './styles.css';
+
+// Normally, we need to encode our API keys, but going along with The Odin Project,
+// this will be learned at a later point in time.
+const apiKey = 'VlDBQdZvUBcEKIWa07x8xnguzSCHYyke';
+
+// Global variables
+const input = document.querySelector('#search');
+const submitBtn = document.querySelector('button[type="submit"]');
+const recentSearches = document.querySelector('.search-history');
+const inputError = document.querySelector('input + span');
+const gifContainer = document.querySelector('.gif-container');
+
+// Focus on the input when page loads
+input.focus();
+
+/**
+ * Capture user input and do the following:
+ * - Put the user input in their recent searches
+ * - Fetch api for gifs
+ * - Display those gifs in the gif container
+ * - Refocus on the input
+ */
+submitBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	// Put the user input in their recent searches
+	if (input.validity.valid) {
+		inputError.classList.remove('active');
+		const inputValue = document.createElement('p');
+		inputValue.textContent = input.value;
+		recentSearches.prepend(inputValue);
+
+		// Have recent searches clickable to research the same gifs
+		document.querySelectorAll('.search-history > p').forEach((el) => {
+			el.addEventListener('click', (e) => {
+				e.preventDefault();
+
+				// Populate input
+				input.value = el.textContent;
+
+				// Make gif container empty
+				gifContainer.textContent = '';
+
+				// fetch data
+				fetchData(input.value);
+
+				// Highlight and select input
+				input.select();
+				input.focus();
+			});
+		});
+
+		// Clear gif container
+		gifContainer.textContent = '';
+
+		// fetch api data
+		fetchData(input.value);
+	} else {
+		inputError.textContent = 'Please enter a search query.';
+		inputError.classList.add('active');
+	}
+
+	// highlight and refocus on the input
+	input.select();
+	input.focus();
+});
+
+/**
+ * Fetches the Giphy API for gifs and returns an array
+ */
+function fetchData(value) {
+	// fetch api data
+	fetch(
+		`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${input.value}`,
+		{
+			mode: 'cors',
+		}
+	)
+		.then((response) => {
+			return response.json();
+		})
+		.then((response) => {
+			// If response came back with zero data
+			if (response.data[0] === undefined) {
+				const nothingToDisplay = document.createElement('p');
+				nothingToDisplay.textContent =
+					'Sorry, the term you have searched came up empty. :(';
+				gifContainer.appendChild(nothingToDisplay);
+			} else {
+				// Data successfully came back
+				response.data.forEach((gif) => {
+					const gifUrl = document.createElement('a');
+					const gifImage = document.createElement('img');
+					gifUrl.setAttribute('href', gif.images.original.url);
+					gifImage.src = gif.images.original.url;
+					gifUrl.appendChild(gifImage);
+					gifContainer.appendChild(gifUrl);
+				});
+			}
+		})
+		.catch((err) => {
+			const error = document.createElement('p');
+			error.textContent = err;
+			gifContainer.appendChild(error);
+		});
+}
